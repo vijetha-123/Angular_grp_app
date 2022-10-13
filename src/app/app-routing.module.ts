0f86d95a39493116826a62f976064ecb7822a1a3
vijetha-123 <dishasankesha@gmail.com>
2022-10-13 17:55:09 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { SignupComponent } from './signup/signup.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+     {path:'Contact',component:ContactComponent},
+     { path:'sign', component:SignupComponent},
+     { path:'subscribe',component:SubscribeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignupComponent } from './signup/signup.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContactComponent,
+    FooterComponent,
+    SignupComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
