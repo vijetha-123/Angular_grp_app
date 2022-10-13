d217a3dc67c35978431234f57c9d8948e4ac08b4
vijetha-123 <dishasankesha@gmail.com>
2022-10-13 21:23:17 +0530

    commit

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -14,16 +14,7 @@ showproductDetailsToggle1()
   this.productDetail1=!this.productDetail1;
 }
 
-toggleContent1(){
-  var content='';
-  if(this.productDetail1){
-    content='fa-solid fa-plus';
-  }
-  else{
-    content='fa-solid fa-minus';
-  }
-  return content;
-}
+
 //222222222222222222222222222
 productDetail2=true;
 //Event to perform the toggle effect
@@ -32,16 +23,7 @@ showproductDetailsToggle2()
   this.productDetail2=!this.productDetail2;
 }
 
-toggleContent2(){
-  var content='';
-  if(this.productDetail2){
-    content='fa-solid fa-plus';
-  }
-  else{
-    content='fa-solid fa-minus';
-  }
-  return content;
-}
+
 //3333333333333333333333333333333
 productDetail3=true;
 //Event to perform the toggle effect
@@ -50,16 +32,7 @@ showproductDetailsToggle3()
   this.productDetail3=!this.productDetail3;
 }
 
-toggleContent3(){
-  var content='';
-  if(this.productDetail3){
-    content='fa-solid fa-plus';
-  }
-  else{
-    content='fa-solid fa-minus';
-  }
-  return content;
-}
+
 //444444444444444444444444444444444
 productDetail4=true;
 //Event to perform the toggle effect
@@ -68,16 +41,7 @@ showproductDetailsToggle4()
   this.productDetail4=!this.productDetail4;
 }
 
-toggleContent4(){
-  var content='';
-  if(this.productDetail4){
-    content='fa-solid fa-plus';
-  }
-  else{
-    content='fa-solid fa-minus';
-  }
-  return content;
-}
+
 //555555555555555555555555555555555
 productDetail5=true;
 //Event to perform the toggle effect
@@ -86,16 +50,7 @@ showproductDetailsToggle5()
   this.productDetail5=!this.productDetail5;
 }
 
-toggleContent5(){
-  var content='';
-  if(this.productDetail5){
-    content='fa-solid fa-plus';
-  }
-  else{
-    content='fa-solid fa-minus';
-  }
-  return content;
-}
+
 //666666666666666666666666666666666
 productDetail6=true;
 //Event to perform the toggle effect
@@ -104,16 +59,7 @@ showproductDetailsToggle6()
   this.productDetail6=!this.productDetail6;
 }
 
-toggleContent6(){
-  var content='';
-  if(this.productDetail6){
-    content='fa-solid fa-plus';
-  }
-  else{
-    content='fa-solid fa-minus';
-  }
-  return content;
-}
+
 
 
   constructor() { }
