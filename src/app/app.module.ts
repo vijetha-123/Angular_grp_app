5537f5d042e71a572bf5d6520ce2e940e50ed10c
swasthika443 <swasthika2409@gmail.com>
2022-10-14 13:00:18 +0530

    blog3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 import { SignupComponent } from './signup/signup.component';
 import { SubscribeComponent } from './subscribe/subscribe.component';
 import { CommonModule } from '@angular/common';
-import { BlogComponent } from './blog/blog.component';
+import{BlogComponent} from './blog/blog.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +16,8 @@ import { BlogComponent } from './blog/blog.component';
     ContactComponent,
     FooterComponent,
     SignupComponent,
-    SubscribeComponent
+    SubscribeComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
