f0760f6785797ab6aa86aa8ef8ff5719455a014a
Varshini77 <varshinikulkarni1@gmail.com>
2022-10-19 11:32:43 +0530

    commit

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -6,6 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent implements OnInit {
+  productDetail1=true;
+//Event to perform the toggle effect
+showproductDetailsToggle1()
+{
+  this.productDetail1=!this.productDetail1;
+}
+productDetail2=true;
+//Event to perform the toggle effect
+showproductDetailsToggle2()
+{
+  this.productDetail2=!this.productDetail2;
+}
+productDetail3=true;
+//Event to perform the toggle effect
+showproductDetailsToggle3()
+{
+  this.productDetail3=!this.productDetail3;
+}
+productDetail4=true;
+//Event to perform the toggle effect
+showproductDetailsToggle4()
+{
+  this.productDetail4=!this.productDetail4;
+}
+productDetail5=true;
+//Event to perform the toggle effect
+showproductDetailsToggle5()
+{
+  this.productDetail5=!this.productDetail5;
+}
+productDetail6=true;
+//Event to perform the toggle effect
+showproductDetailsToggle6()
+{
+  this.productDetail6=!this.productDetail6;
+}
+productDetail7=true;
+//Event to perform the toggle effect
+showproductDetailsToggle7()
+{
+  this.productDetail7=!this.productDetail7;
+}
+productDetail8=true;
+//Event to perform the toggle effect
+showproductDetailsToggle8()
+{
+  this.productDetail8=!this.productDetail8;
+}
+
 
   constructor() { }
 
